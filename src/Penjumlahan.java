7b082c3e9e570881930597763e7ee881a14f88a0
MUHAMMADRYANHIDAYAT <ryan.hidayat.brb@gmail.com>
2022-11-03 01:35:46 +0800

    TUGASPBO2

---
+++ src/Penjumlahan.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author bmb
+ */
+public class Penjumlahan {
+    public static void main (String[] args) {
+        int nilaiA = 1500;
+        int nilaiB = 75;
+        int Jumlah = nilaiA + nilaiB;
+        // Lengkapilah kode pada baris ini
+        System.out.println("Nilai A = " + nilaiA);
+        System.out.println("Nilai B = " + nilaiB);
+        System.out.println("Jumlah  = " + Jumlah);
+        
+                
+    }
+}
